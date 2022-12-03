5ffdd7f1044427bf872da4240ee2f9afc3fad818
ValterMonteiro <valtinms@gmail.com>
2022-12-02 22:49:18 -0400

    🚧 work in progress

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,37 @@
 import '../css/style.css'
 
+/* selecionar os elementos do DOM */
+const inputPesquisa = document.querySelector('#inputPesquisa')
+const btnLocalizar = document.querySelector('#btnLocalizar')
+const pokedexDisplay = document.querySelector('display')
+
+/* adicionando os eventos */
+btnLocalizar.addEventListener('click', async function(){
+    /* buscar os dados na api */
+     const dadosDoPokemon = await localizaPokemon(inputPesquisa.value)
+
+    /* criar o cartao do pokemon */
+    criarCartao()
+})
+
+async function localizaPokemon(termoBusca){
+    const url = `https://pokeapi.co/api/v2/pokemon/${termoBusca}`
+
+    const response = await fetch(url)
+    const pokemon = await response.json()
+    /* console.log(pokemon) */
+    return pokemon
+}
+
+
+function criarCartao(pokemon){
+    const cartaoPokemon = document.createElement('div')
+    cartaoPokemon.className='cartaoPokemon'
+    cartaoPokemon.innerHTML = `
+        <img class="pokemonSprite" src="${pokemon.sprites.front_default}"/>
+        <h2>${pokemon.name}<h2/>
+    `
+
+    /* adicionando ao display */
+    pokedexDisplay.appendChild(cartaoPokemon)
+}
